673259f341beae6287fb643de3db9dc625770e70
Elias Dolinsek (3BHEL) <elias.dolinsek@htl-klu.at>
2021-04-19 09:09:24 +0200

    Add test cases

--- Coffeemachine/src/at/htlklu/coffeemachine/Main.java
+++ Coffeemachine/src/at/htlklu/coffeemachine/Main.java
@@ -3,8 +3,10 @@ package at.htlklu.coffeemachine;
 public class Main {
 
 	public static void main(String[] args) {
-		FilterMachine a = new FilterMachine("Horvat");
-		EspressoMachine b = new EspressoMachine("Robert");
+		FilterMachine a = new FilterMachine("A");
+		EspressoMachine b = new EspressoMachine("B");
+		FilterMachine c = new FilterMachine("C");
+		EspressoMachine d = new EspressoMachine("D");
 		
 		a.fill();
 		a.cook();
@@ -14,5 +16,13 @@ public class Main {
 		b.cook();
 		b.fill();
 		b.cook();
+		
+		c.fill();
+		c.cook();
+		c.cook();
+		
+		d.fill();
+		d.cook();
+		d.cook();
 	}
 }
